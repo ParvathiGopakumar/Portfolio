0f5179c59357658a892a8c71ffe5bf7ea0f757c1
ParvathiGopakumar <145799498+ParvathiGopakumar@users.noreply.github.com>
2024-07-29 22:38:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/* ===================== typing animation ===================== */
+var typed = new Typed(".typing",{
+    strings:["","full stack developer","cloud architect","devops engineer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
